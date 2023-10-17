af5efc8fe94c3a81ac89bd22b621f8cf8415c3a4
Marian Volytskyi <volmark00@gmail.com>
2023-10-17 15:30:29 +0300

    add task solution

--- src/restoreNames.test.js
+++ src/restoreNames.test.js
@@ -1,11 +1,44 @@
 'use strict';
 
 describe('restoreNames', () => {
-  // const { restoreNames } = require('./restoreNames');
+  const { restoreNames } = require('./restoreNames');
 
-  it('should ', () => {
+  it('should restore the missing first name using the fullName', () => {
+    const users = [
+      {
+        firstName: undefined,
+        lastName: 'Holy',
+        fullName: 'Jack Holy',
+      },
+      {
+        lastName: 'Adams',
+        fullName: 'Mike Adams',
+      },
+    ];
 
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('Jack');
+    expect(users[1].firstName).toBe('Mike');
   });
 
-  // write tests here
+  it('should not change the existing first names', () => {
+    const users = [
+      {
+        firstName: 'John',
+        lastName: 'Doe',
+        fullName: 'John Doe',
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Smith',
+        fullName: 'Jane Smith',
+      },
+    ];
+
+    restoreNames(users);
+
+    expect(users[0].firstName).toBe('John');
+    expect(users[1].firstName).toBe('Jane');
+  });
 });
